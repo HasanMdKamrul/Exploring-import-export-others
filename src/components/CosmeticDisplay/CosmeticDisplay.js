6a28dfe90d0280d025d04f0c4a3fe3d428240905
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-09-23 18:58:16 +0200

    Data generated from the json generator and used that as an array

--- src/components/CosmeticDisplay/CosmeticDisplay.js
+++ src/components/CosmeticDisplay/CosmeticDisplay.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const CosmeticDisplay = ({cosmetic}) => {
-    const {name,price,id} = cosmetic;
+    const {name,price,_id} = cosmetic;
     return (
         <div>
             <h2>Name:{name}</h2>
             <h2>Price:{price}</h2>
-            <h2>Id:{id}</h2>
+            <h2>Id:{_id}</h2>
         </div>
     );
 };
--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -5,12 +5,37 @@ const Cosmetics = () => {
     // ** Own fake data rendering
 
     const cosmetics = [
-        {name:'Lipstick',price:200,id:1},
-        {name:'Alta',price:300,id:2},
-        {name:'Nailpalish',price:500,id:3},
-        {name:'Gel',price:200,id:4},
-    
-    ]
+        {
+          "_id": "632de56983da602f61f0d83d",
+          "price": 96,
+          "name": "Schmidt Stewart"
+        },
+        {
+          "_id": "632de569878d8f0869f2bb97",
+          "price": 457,
+          "name": "Millicent Chavez"
+        },
+        {
+          "_id": "632de569d7ca7ae7d7f68c2b",
+          "price": 553,
+          "name": "Lott Hicks"
+        },
+        {
+          "_id": "632de56981c3f690ba71c0f6",
+          "price": 670,
+          "name": "Poole Reese"
+        },
+        {
+          "_id": "632de569656f5e40e1b049e3",
+          "price": 432,
+          "name": "Farley Melton"
+        },
+        {
+          "_id": "632de569a21d8e1a5c83f03f",
+          "price": 74,
+          "name": "Ellison Woodard"
+        }
+      ]
   
     return (
         <div>
